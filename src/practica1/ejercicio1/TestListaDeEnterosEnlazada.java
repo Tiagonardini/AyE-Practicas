078875fa8ae08e571f3a67d594dbd869b91d8dc9
tiagonardini <Tiagonardini15@gmail.com>
2024-08-09 11:03:09 -0300

    TP 1 - ejercicio 1.2

---
+++ src/practica1/ejercicio1/TestListaDeEnterosEnlazada.java
@@ -0,0 +1,22 @@
+package practica1.ejercicio1;
+
+public class TestListaDeEnterosEnlazada {
+    public static void main(String[] args) {
+        var listaDeEnteros = new ListaDeEnterosEnlazada();
+
+        listaDeEnteros.agregarFinal(1);
+        listaDeEnteros.agregarFinal(2);
+        listaDeEnteros.agregarFinal(3);
+        listaDeEnteros.agregarFinal(4);
+        listaDeEnteros.agregarFinal(5);
+
+        listaDeEnteros.comenzar();
+        while (!listaDeEnteros.esVacia()) {
+            if (!listaDeEnteros.fin()) {
+                System.out.printf(listaDeEnteros.proximo().toString());
+
+            }
+
+        }
+    }
+}
